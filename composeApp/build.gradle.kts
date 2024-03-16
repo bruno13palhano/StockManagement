46e6f901f4e0ab2a964d3b6a50ef4048b7bbc744
bruno13palhano <bruno13palhano@gmail.com>
2024-03-15 22:38:02 -0300

    Remove sourceSets for resources

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,7 +45,6 @@ android {
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
         applicationId = "com.bruno13palhano"
