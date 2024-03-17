afea5c1bf9f25a4bd4c3b698f8ad770c01c59928
bruno13palhano <bruno13palhano@gmail.com>
2024-03-17 18:42:01 -0300

    Remove unused imports

--- composeApp/src/commonMain/kotlin/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/HomeScreen.kt
@@ -1,7 +1,6 @@
 package screens
 
 import ViewModel
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,7 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     title: String,
