9a91b0176f618166e11c6b6c94947aa9a6a02c2e
bruno13palhano <bruno13palhano@gmail.com>
2024-03-20 13:59:16 -0300

    Refactor NavBackStackEntry

--- composeApp/src/commonMain/kotlin/navigation/NavBackStackEntry.kt
+++ composeApp/src/commonMain/kotlin/navigation/NavBackStackEntry.kt
@@ -1,24 +1,7 @@
 package navigation
 
-class NavBackStackEntry(
-    val navStack: ArrayDeque<String>,
+data class NavBackStackEntry(
+    val navigationStack: ArrayDeque<String>,
     val destinationsStack: MutableList<Destination>,
-    val argsValues: HashMap<String, String>
-) {
-
-    fun getInt(name: String): Int? {
-        return try { argsValues[name]?.toInt() } catch (e: Exception) { null }
-    }
-
-    fun getLong(name: String): Long? {
-        return try { argsValues[name]?.toLong() } catch (e: Exception) { null }
-    }
-
-    fun getFloat(name: String): Float? {
-        return try { argsValues[name]?.toFloat() } catch (e: Exception) { null }
-    }
-
-    fun getString(name: String): String? {
-        return argsValues[name]
-    }
-}
+    val arguments: NavArgValues
+)
