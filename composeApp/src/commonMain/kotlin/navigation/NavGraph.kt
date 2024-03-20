f0e3f650193d2cf4f3cc78e9a07839813ca6b664
bruno13palhano <bruno13palhano@gmail.com>
2024-03-19 21:19:56 -0300

    Refactor NavGraph

--- composeApp/src/commonMain/kotlin/navigation/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/navigation/NavGraph.kt
@@ -5,9 +5,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 @Composable
 fun NavHost(
@@ -20,12 +17,24 @@ fun NavHost(
 
     val currentRoute by navController.currentRoute().collectAsState(initial = startDestination)
 
-    nav.navController.destinations.firstOrNull { it.route == currentRoute }?.content?.let { it() }
+    nav.navController.navBackStackEntry.destinationsStack
+        .firstOrNull { it.route == currentRoute }?.content?.let { it() }
 }
 
 class NavGraph(val navController: NavController) {
-    fun addDestination(route: String, content: @Composable () -> Unit) {
-        navController.addDestination(Destination(route = route, content = content))
+    fun addDestination(
+        route: String,
+        arguments: List<NamedNavArgument>,
+        content: @Composable () -> Unit
+    ) {
+        navController.addDestination(
+            Destination(
+                route = route,
+                arguments = arguments,
+                argumentsValues = mapOf(),
+                content = content
+            )
+        )
     }
 
     fun addDestination(startDestination: String, route: String) {
@@ -35,8 +44,11 @@ class NavGraph(val navController: NavController) {
 
 inline fun NavGraph.composable(
     route: String,
-    crossinline content: @Composable () -> Unit
-) = addDestination(route = route, content = { content() })
+    arguments: List<NamedNavArgument> = listOf(),
+    crossinline content: @Composable ((NavBackStackEntry) -> Unit)
+) {
+    addDestination(route = route, arguments = arguments, content = { content(navController.navBackStackEntry)} )
+}
 
 inline fun NavGraph.navigation(
     startDestination: String,
@@ -46,53 +58,6 @@ inline fun NavGraph.navigation(
     addDestination(startDestination = startDestination, route = route)
     navGraph()
 }
-
-class NavController {
-    private var navigationStack = ArrayDeque<String>()
-    var destinations = mutableListOf<Destination>()
-        private set
-
-    fun navigate(route: String, popUpInclusive: Boolean = false) {
-        if (popUpInclusive) {
-            while (navigationStack.lastOrNull() != route)
-                navigationStack.removeLastOrNull()
-
-        } else {
-            if (navigationStack.lastOrNull() != route)
-                navigationStack.addLast(route)
-        }
-    }
-
-    fun navigateUp() = navigationStack.removeLastOrNull()
-
-    fun addDestination(destination: Destination) {
-        val routes = destinations.map { it.route }
-
-        if (!routes.contains(destination.route))
-            destinations.add(destination)
-    }
-
-    fun addDestination(startDestination: String, route: String) {
-        if (!navigationStack.contains(startDestination) && navigationStack.isEmpty()) {
-            navigationStack.addLast(startDestination)
-            navigationStack.addLast(route)
-        }
-    }
-
-    fun currentRoute(): Flow<String> {
-        return flow {
-            while (true) {
-                navigationStack.lastOrNull()?.let { emit(it) }
-                delay(175)
-            }
-        }
-    }
-}
-
-data class Destination(
-    val route: String,
-    internal val content: @Composable () -> Unit
-)
 
 @Composable
 fun rememberNavController() = remember { NavController() }
