e6c33ed45a8dd46755190d62b0068352db13df84
bruno13palhano <bruno13palhano@gmail.com>
2024-03-21 12:49:44 -0300

    Refactor Home

--- composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
@@ -1,10 +1,11 @@
 package screens.home
 
-import ViewModel
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.ListItem
@@ -12,52 +13,82 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import data.SaleLocalData
+import data.SaleRepository
+import model.Sale
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.stringResource
+import stockmanagement.composeapp.generated.resources.Res
 
-
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun HomeScreen(
-    title: String,
+fun HomeRoute(
     onItemClick: (id: Long) -> Unit,
-    onIconMenuClick: () -> Unit
+    onIconMenuClick: () -> Unit,
+    onAddButtonClick: () -> Unit,
+    viewModel: HomeViewModel = HomeViewModel(
+        saleRepository = SaleRepository(data = SaleLocalData())
+    )
 ) {
-    val viewModel = ViewModel().getVM()
     val sales by viewModel.sales.collectAsState()
 
     LaunchedEffect(key1 = Unit) {
         viewModel.fetchData()
     }
 
+    HomeScreen(
+        sales = sales,
+        onItemClick = onItemClick,
+        onIconMenuClick = onIconMenuClick,
+        onAddButtonClick = onAddButtonClick
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalResourceApi::class)
+@Composable
+private fun HomeScreen(
+    sales: List<Sale>,
+    onItemClick: (id: Long) -> Unit,
+    onIconMenuClick: () -> Unit,
+    onAddButtonClick: () -> Unit
+) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = title) },
+                title = { Text(text = stringResource(Res.string.home_label)) },
                 navigationIcon = {
                     IconButton(onClick = onIconMenuClick) {
                         Icon(
                             imageVector = Icons.Filled.Menu,
-                            contentDescription = null
+                            contentDescription = stringResource(Res.string.navigate_back_label)
                         )
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(Res.string.add_button_label)
+                )
+            }
         }
     ) {
-        LazyColumn {
+        LazyColumn(modifier = Modifier.padding(it)) {
             items(items = sales.toList(), key = { sale -> sale.id }) { sale ->
                 ListItem(modifier = Modifier
                     .clickable {
                         onItemClick(sale.id)
                     }
                 ) {
-                    Text(text = "${sale.productName}, $title")
+                    Text(text = sale.productName)
                 }
             }
         }
--- composeApp/src/commonMain/kotlin/screens/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/screens/home/HomeViewModel.kt
@@ -1,16 +1,10 @@
-package screens
+package screens.home
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import data.SaleLocalData
 import data.SaleRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import model.Sale
 
@@ -18,13 +12,6 @@ class HomeViewModel(
     private val saleRepository: SaleRepository = SaleRepository(data = SaleLocalData())
 ) {
     var sales = MutableStateFlow<List<Sale>>(emptyList())
-
-    var currentSale by mutableStateOf("")
-        private set
-
-    fun updateCurrentSale(currentSale: String) {
-        this.currentSale = currentSale
-    }
 
     fun fetchData() {
         CoroutineScope(Dispatchers.IO).launch {
