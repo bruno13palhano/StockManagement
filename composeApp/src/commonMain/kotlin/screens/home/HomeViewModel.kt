a6b96aaef4728e75f0d3017d2791380684e150cd
bruno13palhano <bruno13palhano@gmail.com>
2024-03-22 14:50:05 -0300

    Remove default instantiation of SaleRepository

--- composeApp/src/commonMain/kotlin/screens/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/screens/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package screens.home
 
-import data.SaleLocalData
 import data.SaleRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -8,9 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import model.Sale
 
-class HomeViewModel(
-    private val saleRepository: SaleRepository = SaleRepository(data = SaleLocalData())
-) {
+class HomeViewModel(private val saleRepository: SaleRepository) {
     var sales = MutableStateFlow<List<Sale>>(emptyList())
 
     fun fetchData() {
