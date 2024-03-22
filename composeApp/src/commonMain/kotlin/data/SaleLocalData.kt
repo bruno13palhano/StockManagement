c578d5e9d1047858a0c8a8eced02790a3f5fba3c
bruno13palhano <bruno13palhano@gmail.com>
2024-03-22 11:17:54 -0300

    Get local data from database

--- composeApp/src/commonMain/kotlin/data/SaleLocalData.kt
+++ composeApp/src/commonMain/kotlin/data/SaleLocalData.kt
@@ -1,52 +1,48 @@
 package data
 
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import app.cash.sqldelight.coroutines.mapToOne
+import com.bruno13palhano.database.SaleQueries
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import model.Sale
 
-class SaleLocalData : Data {
-    private val sales = mutableListOf<Sale>(
-        Sale(id = 1L, productName = "Homem", productPrice = 123.45F),
-        Sale(id = 2L, productName = "Essencial", productPrice = 88.99F),
-        Sale(id = 3L, productName = "Kaiak", productPrice = 133.75F)
-    )
-
+class SaleLocalData(private val saleQueries: SaleQueries) : Data {
     override suspend fun insert(sale: Sale) {
-        sales.add(sale)
+        saleQueries.insert(
+            productName = sale.productName,
+            productPrice = sale.productPrice.toDouble()
+        )
     }
 
     override suspend fun update(sale: Sale) {
-        for (i in 0 until sales.size) {
-            if (sales[i].id == sale.id ) {
-                sales[i] = sale
-                break
-            }
-        }
+        saleQueries.update(
+            productName = sale.productName,
+            productPrice = sale.productPrice.toDouble(),
+            id = sale.id
+        )
     }
 
     override suspend fun delete(id: Long) {
-        for (i in 0 until sales.size) {
-            if (sales[i].id == id ) {
-                sales.remove(sales[i])
-                break
-            }
-        }
+        saleQueries.delete(id = id)
     }
 
     override fun getById(id: Long): Flow<Sale> {
-        var currentSale = Sale(0L, "", 0F)
-
-        for (i in 0 until sales.size) {
-            if (sales[i].id == id ) {
-                currentSale = sales[i]
-                break
-            }
-        }
-
-        return flowOf(currentSale)
+        return saleQueries.getById(id = id, mapper = ::mapToSale).asFlow().mapToOne(Dispatchers.IO)
     }
 
     override fun getAll(): Flow<List<Sale>> {
-        return flowOf(sales)
+        return saleQueries.getAll(mapper = ::mapToSale).asFlow().mapToList(Dispatchers.IO)
     }
+
+    private fun mapToSale(
+        id: Long,
+        productName: String,
+        productPrice: Double
+    ) = Sale(
+        id = id,
+        productName = productName,
+        productPrice = productPrice.toFloat()
+    )
 }
