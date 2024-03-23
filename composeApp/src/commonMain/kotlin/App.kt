c9b7ce4193eb238b879b02453f447ab4b1be92ae
bruno13palhano <bruno13palhano@gmail.com>
2024-03-22 23:26:33 -0300

    Wrapp navigation with DrawerMenu

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,13 @@
+import androidx.compose.material.DrawerValue
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
 import navigation.MainNavigation
+import navigation.rememberNavController
+import screens.components.Menu
 import screens.home.HomeViewModel
 import screens.sales.SaleViewModel
 
@@ -9,10 +15,23 @@ import screens.sales.SaleViewModel
 fun App(appContainer: AppContainer) {
     MaterialTheme {
         Surface {
-            MainNavigation(
-                homeViewModel = HomeViewModel(appContainer.saleRepository),
-                saleViewModel = SaleViewModel(appContainer.saleRepository)
-            )
+            val drawerState = rememberDrawerState(DrawerValue.Closed)
+            val navController = rememberNavController()
+            val scope = rememberCoroutineScope()
+
+            Menu(drawerState, navController) {
+                MainNavigation(
+                    onIconMenuClick = {
+                        scope.launch {
+                            if (drawerState.isClosed)
+                                drawerState.open()
+                        }
+                    },
+                    navController = navController,
+                    homeViewModel = HomeViewModel(appContainer.saleRepository),
+                    saleViewModel = SaleViewModel(appContainer.saleRepository)
+                )
+            }
         }
     }
 }
