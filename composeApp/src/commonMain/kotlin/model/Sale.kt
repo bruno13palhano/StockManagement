dfa9de3d14c7ad1d08b54661d7fb57b14d7b75b9
bruno13palhano <bruno13palhano@gmail.com>
2024-03-24 23:03:52 -0300

    Add new properties to Sale model

--- composeApp/src/commonMain/kotlin/model/Sale.kt
+++ composeApp/src/commonMain/kotlin/model/Sale.kt
@@ -2,6 +2,24 @@ package model
 
 data class Sale(
     val id: Long,
+    val customerId: Long,
     val productName: String,
-    val productPrice: Float
+    val customerName: String,
+    val purchasePrice: Float,
+    val salePrice: Float,
+    val deliveryPrice: Float,
+    val amazonCode: String,
+    val sku: String,
+    val requestNumber: Long,
+    val tax: Int,
+    val amazonProfit: Float,
+    val resaleProfit : Float,
+    val totalProfit: Float,
+    val dateOfSale: Long,
+    val dateOfPayment: Long,
+    val dateOfShipping: Long,
+    val dateOfDelivery: Long,
+    val isPaid: Boolean,
+    val delivered: Boolean,
+    val canceled: Boolean
 )
