a91ca64d4c454623bc67f49c9813ff12c6e7ba3c
bruno13palhano <bruno13palhano@gmail.com>
2024-03-25 19:06:29 -0300

    Add a field for click

--- composeApp/src/commonMain/kotlin/screens/components/Fields.kt
+++ composeApp/src/commonMain/kotlin/screens/components/Fields.kt
@@ -4,12 +4,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -120,5 +121,35 @@ fun IntegerInputField(
         keyboardActions = KeyboardActions(onDone = {
             defaultKeyboardAction(ImeAction.Done)
         })
+    )
+}
+
+@Composable
+fun ClickField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onClick: () -> Unit,
+    icon: ImageVector,
+    label: String,
+    placeholder: String,
+    singleLine: Boolean = true,
+    readOnly: Boolean = true
+) {
+    OutlinedTextField(
+        modifier = modifier
+            .padding(horizontal = 8.dp, vertical = 2.dp)
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                if (focusState.hasFocus) {
+                    onClick()
+                }
+            },
+        value = value,
+        onValueChange = {},
+        leadingIcon = { Icon(imageVector = icon, contentDescription = label) },
+        label = { Text(text = label, fontStyle = FontStyle.Italic) },
+        placeholder = { Text(text = placeholder, fontStyle = FontStyle.Italic) },
+        singleLine = singleLine,
+        readOnly = readOnly
     )
 }
