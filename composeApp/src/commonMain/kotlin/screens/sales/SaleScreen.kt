63f42ffbfe2e04446a3a94d5ce6b94053b9733a6
bruno13palhano <bruno13palhano@gmail.com>
2024-03-26 13:30:57 -0300

    Set new fields

--- composeApp/src/commonMain/kotlin/screens/sales/SaleScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/sales/SaleScreen.kt
@@ -1,26 +1,60 @@
 package screens.sales
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ListItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AreaChart
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Numbers
 import androidx.compose.material.icons.filled.Paid
+import androidx.compose.material.icons.filled.Percent
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PieChart
+import androidx.compose.material.icons.filled.QrCodeScanner
 import androidx.compose.material.icons.filled.Title
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.stringResource
+import screens.components.BottomSheet
+import screens.components.ClickField
 import screens.components.FloatInputField
+import screens.components.IntegerInputField
 import screens.components.TextInputField
+import screens.currentDate
+import screens.dateFormat
 import stockmanagement.composeapp.generated.resources.Res
 
 @OptIn(ExperimentalResourceApi::class)
@@ -56,7 +90,7 @@ fun EditSaleRoute(
     )
 }
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
 @Composable
 private fun SaleScreen(
     id: Long,
@@ -65,9 +99,153 @@ private fun SaleScreen(
     onBackClick: () -> Unit,
     viewModel: SaleViewModel
 ) {
-    if (id != 0L) {
-        LaunchedEffect(key1 = Unit) {
+
+    LaunchedEffect(key1 = Unit) { viewModel.getAllCustomers() }
+
+    LaunchedEffect(key1 = Unit) {
+        if (id != 0L) {
             viewModel.getSale(id = id)
+        } else {
+            viewModel.updateDateOfSale(currentDate)
+            viewModel.updateDateOfPayment(currentDate)
+            viewModel.updateDateOfShipping(currentDate)
+            viewModel.updateDateOfDelivery(currentDate)
+        }
+    }
+
+    val focusManager = LocalFocusManager.current
+
+    var  openCustomerSheet by remember { mutableStateOf(false) }
+    var dateOfSalePickerState = rememberDatePickerState()
+    var showDateOfSalePickerState by remember { mutableStateOf(false) }
+    var dateOfPaymentPickerState = rememberDatePickerState()
+    var showDateOfPaymentPickerState by remember { mutableStateOf(false) }
+    var dateOfShippingPickerState = rememberDatePickerState()
+    var showDateOfShippingPickerState by remember { mutableStateOf(false) }
+    var dateOfDeliveryPickerState = rememberDatePickerState()
+    var showDateOfDeliveryPickerState by remember { mutableStateOf(false) }
+
+    if (showDateOfSalePickerState) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDateOfSalePickerState = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        dateOfSalePickerState.selectedDateMillis?.let {
+                            viewModel.updateDateOfSale(date = it)
+                        }
+                        focusManager.clearFocus(force = true)
+                        showDateOfSalePickerState = false
+                    }
+                ) {
+                    Text(text = stringResource(Res.string.date_of_sale_label))
+                }
+            }
+        ) {
+            dateOfSalePickerState = rememberDatePickerState(
+                initialSelectedDateMillis = viewModel.dateOfSale,
+                initialDisplayMode = DisplayMode.Picker
+            )
+            DatePicker(
+                state = dateOfSalePickerState,
+                showModeToggle = true
+            )
+        }
+    }
+
+    if (showDateOfPaymentPickerState) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDateOfPaymentPickerState = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        dateOfPaymentPickerState.selectedDateMillis?.let {
+                            viewModel.updateDateOfPayment(date = it)
+                        }
+                        focusManager.clearFocus(force = true)
+                        showDateOfPaymentPickerState = false
+                    }
+                ) {
+                    Text(text = stringResource(Res.string.date_of_payment_label))
+                }
+            }
+        ) {
+            dateOfPaymentPickerState = rememberDatePickerState(
+                initialSelectedDateMillis = viewModel.dateOfPayment,
+                initialDisplayMode = DisplayMode.Picker
+            )
+            DatePicker(
+                state = dateOfPaymentPickerState,
+                showModeToggle = true
+            )
+        }
+    }
+
+    if (showDateOfShippingPickerState) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDateOfShippingPickerState = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        dateOfShippingPickerState.selectedDateMillis?.let {
+                            viewModel.updateDateOfShipping(date = it)
+                        }
+                        focusManager.clearFocus(force = true)
+                        showDateOfShippingPickerState = false
+                    }
+                ) {
+                    Text(text = stringResource(Res.string.date_of_shipping_label))
+                }
+            }
+        ) {
+            dateOfShippingPickerState = rememberDatePickerState(
+                initialSelectedDateMillis = viewModel.dateOfShipping,
+                initialDisplayMode = DisplayMode.Picker
+            )
+            DatePicker(
+                state = dateOfShippingPickerState,
+                showModeToggle = true
+            )
+        }
+    }
+
+    if (showDateOfDeliveryPickerState) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDateOfDeliveryPickerState = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        dateOfDeliveryPickerState.selectedDateMillis?.let {
+                            viewModel.updateDateOfDelivery(date = it)
+                        }
+                        focusManager.clearFocus(force = true)
+                        showDateOfDeliveryPickerState = false
+                    }
+                ) {
+                    Text(text = stringResource(Res.string.date_of_delivery_label))
+                }
+            }
+        ) {
+            dateOfDeliveryPickerState = rememberDatePickerState(
+                initialSelectedDateMillis = viewModel.dateOfDelivery,
+                initialDisplayMode = DisplayMode.Picker
+            )
+            DatePicker(
+                state = dateOfDeliveryPickerState,
+                showModeToggle = true
+            )
         }
     }
 
@@ -99,7 +277,43 @@ private fun SaleScreen(
             }
         }
     ) {
-        Column(modifier = Modifier.padding(it).padding(vertical = 4.dp)) {
+        Column(
+            modifier = Modifier
+                .padding(it).padding(vertical = 4.dp)
+                .verticalScroll(state = rememberScrollState(), enabled = true)
+        ) {
+            AnimatedVisibility(visible = openCustomerSheet) {
+                BottomSheet(
+                    onDismissBottomSheet = {
+                        openCustomerSheet = false
+                        focusManager.clearFocus(force = true)
+                    }
+                ) {
+                    val initialCustomer = viewModel.allCustomers
+                        .filter { customer -> customer.isChecked }
+                        .findLast { customer -> customer.isChecked }?.name
+                    val (selected, onOptionSelected) = rememberSaveable {
+                        mutableStateOf(initialCustomer)
+                    }
+
+                    Column(modifier = Modifier.padding(bottom = 32.dp)) {
+                        viewModel.allCustomers.forEach { customerCheck ->
+                            ListItem(
+                                headlineContent = { Text(text = customerCheck.name) },
+                                leadingContent = {
+                                    RadioButton(
+                                        selected = customerCheck.name == selected,
+                                        onClick = {
+                                            onOptionSelected(customerCheck.name)
+                                            viewModel.updateCustomerName(customerCheck.name)
+                                        }
+                                    )
+                                }
+                            )
+                        }
+                    }
+                }
+            }
             TextInputField(
                 value = viewModel.productName,
                 onValueChange = viewModel::updateProductName,
@@ -107,13 +321,129 @@ private fun SaleScreen(
                 label = stringResource(Res.string.product_name_label),
                 placeholder = stringResource(Res.string.enter_product_name_label)
             )
-            FloatInputField(
-                value = viewModel.productPrice,
-                onValueChange = viewModel::updateProductPrice,
-                icon = Icons.Filled.Paid,
-                label = stringResource(Res.string.product_price_label),
-                placeholder = stringResource(Res.string.enter_product_price_label)
+            ClickField(
+                value = viewModel.customerName,
+                onClick = { openCustomerSheet = true },
+                icon = Icons.Filled.Person,
+                label = stringResource(Res.string.customer_name_label),
+                placeholder = ""
             )
+            FloatInputField(
+                value = viewModel.purchasePrice,
+                onValueChange = viewModel::updatePurchasePrice,
+                icon = Icons.Filled.Paid,
+                label = stringResource(Res.string.purchase_price_label),
+                placeholder = stringResource(Res.string.enter_purchase_price_label)
+            )
+            FloatInputField(
+                value = viewModel.salePrice,
+                onValueChange = viewModel::updateSalePrice,
+                icon = Icons.Filled.Paid,
+                label = stringResource(Res.string.sale_price_label),
+                placeholder = stringResource(Res.string.enter_sale_price_label)
+            )
+            FloatInputField(
+                value = viewModel.deliveryPrice,
+                onValueChange = viewModel::updateDeliveryPrice,
+                icon = Icons.Filled.Paid,
+                label = stringResource(Res.string.delivery_price_label),
+                placeholder = stringResource(Res.string.enter_delivery_price_label)
+            )
+            TextInputField(
+                value = viewModel.amazonCode,
+                onValueChange = viewModel::updateAmazonCode,
+                icon = Icons.Filled.Code,
+                label = stringResource(Res.string.amazon_code_label),
+                placeholder = stringResource(Res.string.enter_amazon_code_label)
+            )
+            TextInputField(
+                value = viewModel.sku,
+                onValueChange = viewModel::updateSku,
+                icon = Icons.Filled.QrCodeScanner,
+                label = stringResource(Res.string.sku_label),
+                placeholder = stringResource(Res.string.enter_sku_label)
+            )
+            IntegerInputField(
+                value = viewModel.requestNumber,
+                onValueChange = viewModel::updateRequestNumber,
+                icon = Icons.Filled.Numbers,
+                label = stringResource(Res.string.request_number_label),
+                placeholder = stringResource(Res.string.enter_request_number_label)
+            )
+            TextInputField(
+                value = viewModel.tax,
+                onValueChange = viewModel::updateTax,
+                icon = Icons.Filled.Percent,
+                label = stringResource(Res.string.tax_label),
+                placeholder = stringResource(Res.string.enter_tax_label)
+            )
+            FloatInputField(
+                value = viewModel.amazonProfit,
+                onValueChange = viewModel::updateAmazonProfit,
+                icon = Icons.Filled.BarChart,
+                label = stringResource(Res.string.amazon_profit_label),
+                placeholder = ""
+            )
+            FloatInputField(
+                value = viewModel.resaleProfit,
+                onValueChange = viewModel::updateResaleProfit,
+                icon = Icons.Filled.AreaChart,
+                label = stringResource(Res.string.resale_profit_label),
+                placeholder = stringResource(Res.string.enter_resale_profit_label)
+            )
+            FloatInputField(
+                value = viewModel.totalProfit,
+                onValueChange = viewModel::updateTotalProfit,
+                icon = Icons.Filled.PieChart,
+                label = stringResource(Res.string.total_profit_label),
+                placeholder = ""
+            )
+            ClickField(
+                value = dateFormat.format(viewModel.dateOfSale),
+                onClick = { showDateOfSalePickerState = true },
+                icon = Icons.Filled.CalendarToday,
+                label = stringResource(Res.string.date_of_sale_label),
+                placeholder = ""
+            )
+            ClickField(
+                value = dateFormat.format(viewModel.dateOfPayment),
+                onClick = { showDateOfPaymentPickerState = true },
+                icon = Icons.Filled.CalendarToday,
+                label = stringResource(Res.string.date_of_payment_label),
+                placeholder = ""
+            )
+            ClickField(
+                value = dateFormat.format(viewModel.dateOfShipping),
+                onClick = { showDateOfShippingPickerState = true },
+                icon = Icons.Filled.CalendarToday,
+                label = stringResource(Res.string.date_of_shipping_label),
+                placeholder = ""
+            )
+            ClickField(
+                value = dateFormat.format(viewModel.dateOfDelivery),
+                onClick = { showDateOfDeliveryPickerState = true },
+                icon = Icons.Filled.CalendarToday,
+                label = stringResource(Res.string.date_of_delivery_label),
+                placeholder = ""
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = viewModel.isPaid,
+                    onCheckedChange = viewModel::updateIsPaid
+                )
+                Text(text = stringResource(Res.string.is_paid_label))
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = viewModel.delivered,
+                    onCheckedChange = viewModel::updateDelivered
+                )
+                Text(text = stringResource(Res.string.delivered_label))
+            }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/screens/sales/SaleViewModel.kt
+++ composeApp/src/commonMain/kotlin/screens/sales/SaleViewModel.kt
@@ -1,8 +1,10 @@
 package screens.sales
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import data.CustomerRepository
 import data.SaleRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
@@ -13,37 +15,192 @@ import model.Sale
 
 class SaleViewModel(
     private val saleRepository: SaleRepository,
+    private val customerRepository: CustomerRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
+    private var customerId = 0L
+    private var canceled = false
+
     var productName by mutableStateOf("")
         private set
-    var productPrice by mutableStateOf("")
+    var customerName by mutableStateOf("")
+        private set
+    var purchasePrice by mutableStateOf("")
+        private set
+    var salePrice by mutableStateOf("")
+        private set
+    var deliveryPrice by mutableStateOf("")
+        private set
+    var amazonCode by mutableStateOf("")
+        private set
+    var sku by mutableStateOf("")
+        private set
+    var requestNumber by mutableStateOf("")
+        private set
+    var tax by mutableStateOf("")
+        private set
+    var amazonProfit by mutableStateOf("")
+        private set
+    var resaleProfit by mutableStateOf("")
+        private set
+    var totalProfit by mutableStateOf("")
+        private set
+    var dateOfSale by mutableLongStateOf(0L)
+        private set
+    var dateOfPayment by mutableLongStateOf(0L)
+        private set
+    var dateOfShipping by mutableLongStateOf(0L)
+        private set
+    var dateOfDelivery by mutableLongStateOf(0L)
+        private set
+    var isPaid by mutableStateOf(false)
+        private set
+    var delivered by mutableStateOf(false)
+        private set
+    var allCustomers by mutableStateOf(listOf<CustomerCheck>())
         private set
 
-    fun updateProductName(productName: String) {
-        this.productName = productName
-    }
+    fun updateProductName(productName: String) { this.productName = productName }
 
-    fun updateProductPrice(productPrice: String) {
-        this.productPrice = productPrice
+    fun updatePurchasePrice(purchasePrice: String) { this.purchasePrice = purchasePrice }
+
+    fun updateSalePrice(salePrice: String) { this.salePrice = salePrice }
+
+    fun updateDeliveryPrice(deliveryPrice: String) { this.deliveryPrice = deliveryPrice }
+
+    fun updateAmazonCode(amazonCode: String) { this.amazonCode = amazonCode }
+
+    fun updateSku(sku: String) { this.sku = sku }
+
+    fun updateRequestNumber(requestNumber: String) { this.requestNumber = requestNumber }
+
+    fun updateTax(tax: String) { this.tax = tax }
+
+    fun updateAmazonProfit(amazonProfit: String) { this.amazonProfit = amazonProfit }
+
+    fun updateResaleProfit(resaleProfit: String) { this.resaleProfit = resaleProfit }
+
+    fun updateTotalProfit(totalProfit: String) { this.totalProfit = totalProfit }
+
+    fun updateDateOfSale(date: Long) { dateOfSale = date }
+
+    fun updateDateOfPayment(date: Long) { dateOfPayment = date }
+
+    fun updateDateOfShipping(date: Long) { dateOfShipping = date }
+
+    fun updateDateOfDelivery(date: Long) { dateOfDelivery = date }
+
+    fun updateIsPaid(isPaid: Boolean) { this.isPaid = isPaid }
+
+    fun updateDelivered(delivered: Boolean) { this.delivered = delivered }
+
+    fun updateCustomerName(customerName: String) {
+        this.customerName = customerName
+        allCustomers.map { customer -> customer.isChecked = false }
+        for (customer in allCustomers) {
+            if (customer.name == customerName) {
+                customerId = customer.id
+                customer.isChecked = true
+                break
+            }
+        }
     }
 
     fun getSale(id: Long) {
         CoroutineScope(SupervisorJob() + dispatcher).launch {
             saleRepository.getById(id = id).collect {
+                customerId = it.customerId
                 productName = it.productName
-                productPrice = it.productPrice.toString()
+                customerName = it.customerName
+                purchasePrice = it.purchasePrice.toString()
+                salePrice = it.salePrice.toString()
+                deliveryPrice = it.deliveryPrice.toString()
+                amazonCode = it.amazonCode
+                sku = it.sku
+                requestNumber = it.requestNumber.toString()
+                tax = it.tax.toString()
+                amazonProfit = it.amazonProfit.toString()
+                resaleProfit = it.amazonProfit.toString()
+                totalProfit = it.totalProfit.toString()
+                updateDateOfSale(it.dateOfSale)
+                updateDateOfPayment(it.dateOfPayment)
+                updateDateOfShipping(it.dateOfShipping)
+                updateDateOfDelivery(it.dateOfDelivery)
+                isPaid = it.isPaid
+                delivered = it.delivered
+                canceled = it.canceled
+                setCustomerChecked(customerId = it.customerId)
             }
         }
+    }
+
+    private fun setCustomerChecked(customerId: Long) {
+        for (customer in allCustomers) {
+            if (customer.id == customerId) {
+                customer.isChecked = true
+                break
+            }
+        }
+    }
+
+    fun getAllCustomers() {
+        CoroutineScope(SupervisorJob() + dispatcher).launch {
+            customerRepository.getAll().collect {
+                allCustomers = it.map { customer ->
+                    CustomerCheck(
+                        id = customer.id,
+                        name = customer.name,
+                        isChecked = false
+                    )
+                }
+            }
+        }
+    }
+
+    fun cancelSale(id: Long) {
+        canceled = true
+        save(id = id)
     }
 
     fun save(id: Long) {
         CoroutineScope(SupervisorJob() + dispatcher).launch {
             if (id == 0L) {
-                saleRepository.insert(Sale(id = 0L, productName, productPrice.toFloat()))
+                saleRepository.insert(model = buildSale())
             } else {
-                saleRepository.update(Sale(id, productName, productPrice.toFloat()))
+                saleRepository.update(model = buildSale(id = id))
             }
         }
     }
+
+    private fun buildSale(id: Long = 0L): Sale {
+        return Sale(
+            id = id,
+            customerId = customerId,
+            productName = productName,
+            customerName = customerName,
+            purchasePrice = purchasePrice.toFloat(),
+            salePrice = salePrice.toFloat(),
+            deliveryPrice = deliveryPrice.toFloat(),
+            amazonCode = amazonCode,
+            sku = sku,
+            requestNumber = requestNumber.toLong(),
+            tax = tax.toInt(),
+            amazonProfit = amazonProfit.toFloat(),
+            resaleProfit = resaleProfit.toFloat(),
+            totalProfit = totalProfit.toFloat(),
+            dateOfSale = dateOfSale,
+            dateOfPayment = dateOfPayment,
+            dateOfShipping = dateOfShipping,
+            dateOfDelivery = dateOfDelivery,
+            isPaid = isPaid,
+            delivered = delivered,
+            canceled = canceled
+        )
+    }
 }
+
+data class CustomerCheck(
+    var id: Long,
+    var name: String,
+    var isChecked: Boolean
+)
