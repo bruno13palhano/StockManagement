9dcabee79f15f08122232c8f7f45bed4c4c26433
bruno13palhano <bruno13palhano@gmail.com>
2024-03-27 20:39:45 -0300

    Remove unused imports

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -2,8 +2,6 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.bruno13palhano.DriverFactory
-import com.bruno13palhano.createDatabase
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "StockManagement") {
