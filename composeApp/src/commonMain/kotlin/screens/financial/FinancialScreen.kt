51b716c859476c8495dfa3fd6e5880670cdf6660
bruno13palhano <bruno13palhano@gmail.com>
2024-04-07 15:17:01 -0300

    Get last sales

--- composeApp/src/commonMain/kotlin/screens/financial/FinancialScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/financial/FinancialScreen.kt
@@ -1,7 +1,14 @@
 package screens.financial
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -9,23 +16,41 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import model.Sale
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.stringResource
 import stockmanagement.composeapp.generated.resources.Res
 
 @Composable
 fun FinancialRoute(
-    onIconMenuClick: () -> Unit
+    onIconMenuClick: () -> Unit,
+    viewModel: FinancialViewModel
 ) {
-    FinancialScreen(onIconMenuClick = onIconMenuClick)
+    LaunchedEffect(key1 = Unit) { viewModel.getLastSales() }
+
+    val lastSales by viewModel.lastSales.collectAsState()
+
+    FinancialScreen(
+        lastSales = lastSales,
+        onIconMenuClick = onIconMenuClick
+    )
 }
 
 @OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
 @Composable
 private fun FinancialScreen(
+    lastSales: List<Sale>,
     onIconMenuClick: () -> Unit
 ) {
     Scaffold(
@@ -44,7 +69,45 @@ private fun FinancialScreen(
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
-
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                items(items = lastSales, key = {sale -> sale.id} ) { sale ->
+                    ElevatedCard(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .size(144.dp),
+                        shape = RoundedCornerShape(5),
+                        onClick = {}
+                    ) {
+                        ListItem(
+                            modifier = Modifier
+                                .padding(8.dp),
+                            headlineContent = {
+                                Text(
+                                    text = sale.productName,
+                                    style = MaterialTheme.typography.titleMedium,
+                                    overflow = TextOverflow.Ellipsis,
+                                    maxLines = 1
+                                )
+                                Text(
+                                    text = sale.salePrice.toString(),
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    maxLines = 1
+                                )
+                                Text(
+                                    text = sale.customerName,
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    fontStyle = FontStyle.Italic,
+                                    overflow = TextOverflow.Ellipsis,
+                                    maxLines = 1
+                                )
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
