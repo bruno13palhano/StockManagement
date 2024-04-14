b0123d79de99f0b4cf872f19692fb8f236527afd
bruno13palhano <bruno13palhano@gmail.com>
2024-04-14 20:26:07 -0300

    Add a class for Financial UI state

--- composeApp/src/commonMain/kotlin/screens/financial/FinancialViewModel.kt
+++ composeApp/src/commonMain/kotlin/screens/financial/FinancialViewModel.kt
@@ -15,6 +15,14 @@ class FinancialViewModel(
     private val saleRepository: SaleRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
+    private var _uiState = MutableStateFlow(FinancialUiState())
+    val uiState = _uiState
+        .stateIn(
+            scope = CoroutineScope(SupervisorJob() + dispatcher),
+            started = WhileSubscribed(5_000),
+            initialValue = FinancialUiState()
+        )
+
     private var _lastSales = MutableStateFlow(listOf<Sale>())
 
     val lastSales = _lastSales
@@ -31,4 +39,38 @@ class FinancialViewModel(
             }
         }
     }
+
+    fun getData() {
+        CoroutineScope(SupervisorJob() + dispatcher).launch {
+            saleRepository.getAll().collect {
+                var amount = 0F
+                var profit = 0F
+                var biggestSale = Float.MIN_VALUE
+                var smallestSale = Float.MAX_VALUE
+
+                it.forEach { sale ->
+                    amount += (sale.salePrice * sale.quantity)
+                    profit += ((sale.salePrice * sale.quantity) - sale.deliveryPrice)
+                    if (sale.salePrice >= biggestSale)
+                        biggestSale = sale.salePrice
+                    if (sale.salePrice <= smallestSale)
+                        smallestSale = sale.salePrice
+                }
+
+                _uiState.value = FinancialUiState(
+                    amount = amount,
+                    profit = profit,
+                    biggestSale = biggestSale,
+                    smallestSale = smallestSale
+                )
+            }
+        }
+    }
+
+    data class FinancialUiState(
+        val amount: Float = 0F,
+        val profit: Float = 0F,
+        val biggestSale: Float = 0F,
+        val smallestSale: Float = 0F
+    )
 }
