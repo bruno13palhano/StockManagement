25d04c9bbabee774d2ff3bae7065d6aaaadf7f64
bruno13palhano <bruno13palhano@gmail.com>
2024-04-18 20:01:54 -0300

    Add city, address and phone number to item list

--- composeApp/src/commonMain/kotlin/screens/customer/CustomersScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/customer/CustomersScreen.kt
@@ -18,11 +18,13 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import model.Customer
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -89,7 +91,35 @@ private fun CustomersScreen(
                     shape = RoundedCornerShape(5),
                     onClick = { onItemClick(customer.id) }
                 ) {
-                    ListItem(headlineContent = { Text(text = customer.name) })
+                    ListItem(
+                        headlineContent = {
+                            Text(
+                                text = customer.name,
+                                style = MaterialTheme.typography.titleMedium,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                            Text(
+                                text = stringResource(
+                                    Res.string.double_description_tag,
+                                    customer.city,
+                                    customer.address
+                                ),
+                                style = MaterialTheme.typography.bodyMedium,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                            Text(
+                                text = stringResource(
+                                    Res.string.phone_number_tag,
+                                    customer.phoneNumber
+                                ),
+                                style = MaterialTheme.typography.bodyMedium,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        }
+                    )
                 }
             }
         }
