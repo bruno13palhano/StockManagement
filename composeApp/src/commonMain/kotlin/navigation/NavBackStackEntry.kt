774a3e0f3d9dcaa59d9db0ac19496a1695a9f4eb
bruno13palhano <bruno13palhano@gmail.com>
2024-05-05 13:17:30 -0300

    Remove unused classes

--- composeApp/src/commonMain/kotlin/navigation/NavBackStackEntry.kt
+++
@@ -1,7 +0,0 @@
-package navigation
-
-data class NavBackStackEntry(
-    val navigationStack: ArrayDeque<String>,
-    val destinationsStack: MutableList<Destination>,
-    val arguments: NavArgValues
-)
